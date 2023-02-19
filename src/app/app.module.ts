56ed8d10ffdcd8f8189c161016973950848e2471
Nimisha Wilson <nimishawilson123@gmail.com>
2023-02-19 12:49:14 +0530

    feat: add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitialLoadingComponent } from './initial-loading/initial-loading.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitialLoadingComponent
+    InitialLoadingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
