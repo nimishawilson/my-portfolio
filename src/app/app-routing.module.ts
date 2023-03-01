518b0782e472e5abfaad7a9a03de0e98439d8321
Nimisha Wilson <nimishawilson123@gmail.com>
2023-03-02 00:27:02 +0530

    feat: style project card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 import { ResumeComponent } from './resume/resume.component';
+import { SkillsetAndWorksComponent } from './skillset-and-works/skillset-and-works.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'resume', component: ResumeComponent },
+  { path: 'skills-projects', component: SkillsetAndWorksComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
