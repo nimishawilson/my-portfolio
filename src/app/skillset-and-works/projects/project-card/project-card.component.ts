a0a3de7538adcccce55a2ea01c8e940d2bd1605a
Nimisha Wilson <nimishawilson123@gmail.com>
2023-03-02 00:39:18 +0530

    feat: create and bind properties for project card component

--- src/app/skillset-and-works/projects/project-card/project-card.component.ts
+++ src/app/skillset-and-works/projects/project-card/project-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.scss']
+  styleUrls: ['./project-card.component.scss'],
 })
 export class ProjectCardComponent {
-
+  @Input() projectHeading: string = 'Access Line Integrated Intake System';
+  @Input()
+  content: string = ` A system for recording the cases from clients and assign available attorneys to them.
+ Integrated calendar component library that can display events on a month, week or day view.
+ Created reusable custom components and implement custom validations`;
+  @Input() technologiesUsed: string[] = [
+    'Angular 13',
+    'PrimeFlex',
+    'PrimeNG',
+    'PrimeNG',
+    'Angular 13',
+  ];
 }
